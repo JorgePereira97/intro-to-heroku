5518d4c15213f6f695834e92000b17ac6fcad3ff
JorgePereira97 <53755372+JorgePereira97@users.noreply.github.com>
2019-10-01 14:21:57 +0100

    Signed-off-by: JorgePereira97 <53755372+JorgePereira97@users.noreply.github.com>

--- worker.py
+++ worker.py
@@ -1,5 +1,6 @@
 import psycopg2
 import time
+import os
 
 '''connection = psycopg2.connect(user = "isneqpdtrrraup",
                               password = "d6f221a93bf2e038e37179cf4e0d4a18c321241340b0bf4368439fa212fc9b06",
@@ -7,7 +8,9 @@ import time
                               port = "5432",
                               database = "delcjv8nidnnch")'''
 
-connection = psycopg2.connect("postgres://isneqpdtrrraup:d6f221a93bf2e038e37179cf4e0d4a18c321241340b0bf4368439fa212fc9b06@ec2-176-34-183-20.eu-west-1.compute.amazonaws.com:5432/delcjv8nidnnch")
+#connection = psycopg2.connect("postgres://isneqpdtrrraup:d6f221a93bf2e038e37179cf4e0d4a18c321241340b0bf4368439fa212fc9b06@ec2-176-34-183-20.eu-west-1.compute.amazonaws.com:5432/delcjv8nidnnch")
+
+connection = psycopg2.connect(os.environ['DATABASE_URL'])
 
 cursor = connection.cursor()
 
